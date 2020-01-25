fa93770adf782e6ddb3f2e5ef443c2b121111630
yarchike <56361599+yarchike@users.noreply.github.com>
2020-01-25 16:36:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/work222q/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.work222q;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Intent intentNotes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch(id){
+            case R.id.action_open_notes:
+            Toast.makeText(MainActivity.this, "Отрыть записную книжку", Toast.LENGTH_LONG).show();
+            intentNotes = new Intent(MainActivity.this, NotesActivity.class);
+            startActivity(intentNotes);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ app/src/main/java/com/example/work222q/NotesActivity.java
@@ -0,0 +1,59 @@
+package com.example.work222q;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class NotesActivity extends AppCompatActivity {
+
+    private EditText mInputNote;
+    private Button mBtnSaveNote;
+    private SharedPreferences myNoteSharedPref;
+    private static String NOTE_TEXT = "note_text";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        initViews();
+        getDateFromSharedPref();
+    }
+
+    private void initViews() {
+        mInputNote = findViewById(R.id.inputNote);
+        mBtnSaveNote = findViewById(R.id.btnSaveNote);
+        myNoteSharedPref = getSharedPreferences("Myfile", MODE_PRIVATE);
+
+
+       mBtnSaveNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor myEditor = myNoteSharedPref.edit();
+                String noteTXT = mInputNote.getText().toString();
+                myEditor.putString(NOTE_TEXT, noteTXT);
+                Boolean p = myEditor.commit();
+                if (p) {
+                    Toast.makeText(NotesActivity.this, getText(R.string.save_data), Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(NotesActivity.this, getText(R.string.nosave_data), Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+    private void getDateFromSharedPref(){
+        String noteTxt = myNoteSharedPref.getString(NOTE_TEXT, "");
+        mInputNote.setText(noteTxt);
+    }
+}
